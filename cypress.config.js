9781706e401dcd44cc19bd0ccd470e9821250eca
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-04-04 14:24:22 +0200

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    video: false,
+  },
+});
