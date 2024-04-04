bc93004656878330dffc083c7c282a89d6dc9598
Tolok Oleksandr <tolok.oleksandr@gmail.com>
2024-04-04 16:00:01 +0200

    Mocking GET Requests by Stubbing Responses

--- cypress/e2e/network-request.cy.js
+++ cypress/e2e/network-request.cy.js
@@ -9,10 +9,22 @@ describe("Network Request", () => {
 
   it("Get Request", () => {
     // Listening for GET request, that contain  url: "**/comments/*".
-    cy.intercept({
-      method: "GET",
-      url: "**/comments/*",
-    }).as("getComment");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "**/comments/*",
+      },
+      // Mocking Get Request
+      {
+        body: {
+          postId: 1,
+          id: 1,
+          name: "test name 123",
+          email: "test@gmail.com",
+          body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard",
+        },
+      }
+    ).as("getComment");
 
     // Clicking on button
     cy.get(".network-btn").click();
